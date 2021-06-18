97eb8924ad71bb923b386692e50aab8267ff04cd
Gianluca <gl.galassini@gmail.com>
2021-06-18 16:39:27 +0200

    aggiungo la funzione che sceglie in base all'indice

--- js/script.js
+++ js/script.js
@@ -2,18 +2,27 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-const getListByElement = (x, a, b) => {
+// const getListByElement = (x, a, b) => {
+//     while (a > b) {
+//         a= b-1;
+//     }
+//    return x.filter((e) => { 
+//        return (e> a && e< b ||e>= a && e< b||e> a && e<= b)? true : false
+//     })
+// };
+
+const getListByIndex = (x, a, b) => {
     while (a > b) {
         a= b-1;
     }
-   return x.filter((num) => { 
-       return (num> a && num< b ||num>= a && num< b||num> a && num<= b)? true : false
+   return x.filter((noUse, i ) => { 
+       return (i> a && i< b ||i>= a && i< b||i> a && i<= b)? true : false
     })
 };
 
 const startingList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const test = getListByElement(startingList, 3, 10);
-const testWhile = getListByElement(startingList, 10, 3);
+const test = getListByIndex(startingList, 3, 10);
+const testWhile = getListByIndex(startingList, 10, 3);
 const result = 
     `
         <h2>Risultato con a minore di b</h2><br>
