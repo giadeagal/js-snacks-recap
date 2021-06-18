e7578baff557d7b1c61bd9198a3f3bcc2dc17df0
Gianluca <gl.galassini@gmail.com>
2021-06-18 16:36:32 +0200

    cambio il nome della funzione

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-const getList = (x, a, b) => {
+const getListByElement = (x, a, b) => {
     while (a > b) {
         a= b-1;
     }
@@ -12,8 +12,8 @@ const getList = (x, a, b) => {
 };
 
 const startingList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const test = getList(startingList, 3, 10);
-const testWhile = getList(startingList, 10, 3);
+const test = getListByElement(startingList, 3, 10);
+const testWhile = getListByElement(startingList, 10, 3);
 const result = 
     `
         <h2>Risultato con a minore di b</h2><br>
